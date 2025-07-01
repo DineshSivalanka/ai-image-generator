cc9087fb7aea7e7e391717b0dc48079ad4dd20d4
Dinesh Venkata Kumar Sivalanka <sivalankadinesh2005@gmail.com>
2025-07-01 23:00:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,4 +25,4 @@ app.post('/generate', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('Backend running on port ${PORT}'));
+app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
